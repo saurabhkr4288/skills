c69e6f6175e8c6d9f887b6682de89b64ea3fd86a
saurabhkr4288 <saurabhkr4288@gmail.com>
2026-06-18 11:54:06 +0530

    Update hello.cpp

---
+++ DSA/day1/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo"<<endl;
+    cout << "Helic7447lo";
+    return 0;
+}
